17b6bccf155de18cca29c13c82c3783c6f4bd568
fearamire <sambo.jones15@gmail.com>
2024-03-13 09:54:09 -0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,51 @@
+use std::io;
+
+fn main() {
+    let mut a: u32 = 0;
+    let mut b: u32 = 1;
+    let mut c: u32 = 0;
+    let mut i = 2;
+
+    loop {
+        let mut max = String::new();
+
+        // Get index from user
+        print!("Please enter the index of the fibonacci number you would like.\n");
+
+        io::stdin()
+        .read_line(&mut max)
+        .expect("Failed to read line.");
+
+        let max:u32 = match max.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                print!("That wasn't a number.\n\n");
+                continue
+            }
+        };
+
+        print!("\n\n");
+
+        if max == a {
+            print!("The {max} index Fibonacci number is: {a}")
+        } else if max == b {
+            print!("The {max} index Fibonacci number is: {b}")
+        } else {
+            // iterate through and find the requested value
+            while i <= max {
+                c = a + b;
+                a = b;
+                b = c;
+
+                i += 1;
+            };
+            print!("The {max} index Fibonacci number is: {b}");
+        };
+
+        println!("\n\nPress Enter to Exit...");
+        let mut exit = String::new();
+        io::stdin().read_line(&mut exit).unwrap();
+        break
+    }
+    
+}
